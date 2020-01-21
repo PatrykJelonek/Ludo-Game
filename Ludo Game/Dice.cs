11494c5f9a9057bd5dafacdbed3eea237100f76d
PatrykJelonek <ghostzoombi@gmail.com>
2020-01-21 22:13:19 +0100

    Dice Class

---
+++ Ludo Game/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo_Game
+{
+    class Dice
+    {
+        private const int MAX_VALUE = 6;
+
+        private readonly Random random;
+
+        public Dice(Random random)
+        {
+            this.random = random;
+        }
+
+        public int Roll()
+        {
+            return this.random.Next(Dice.MAX_VALUE) + 1;
+        }
+    }
+}
